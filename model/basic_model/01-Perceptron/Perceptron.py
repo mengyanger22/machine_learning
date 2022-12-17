91e79ad7754ae8cfbac1bb61a84ddc1afe472d1b
mengyanger <mengyanger2@gmail.com>
2022-12-17 18:36:48 +0800

    perceptron

--- model/basic_model/01-Perceptron/Perceptron.py
+++ model/basic_model/01-Perceptron/Perceptron.py
@@ -11,3 +11,20 @@ class Perceptron(nn.Module):
             return +1.0
         else:
             return -1.0
+
+class PerceptronLiHangBook(nn.Module):
+    def __init__(self, w, b=0.0):
+        super(PerceptronLiHangBook, self).__init__()
+        self.w = w
+        self.b = b
+    def forward(self, x):
+        return torch.matmul(self.w, x) + self.b
+
+
+class PerceptronMNIST(nn.Module):
+    def __init__(self, w, b=0.0):
+        super(PerceptronMNIST, self).__init__()
+        self.w = w
+        self.b = b
+    def forward(self, x):
+        pass
--- model/basic_model/01-Perceptron/main.py
+++ model/basic_model/01-Perceptron/main.py
@@ -1,9 +1,52 @@
-from Perceptron import Perceptron
+import torch
+
+from Perceptron import Perceptron, PerceptronLiHangBook
+
+def basic():
+    w = torch.tensor([0., 0.], requires_grad=False)
+    b = torch.tensor(0.0, requires_grad=False)
+    model = Perceptron(w, b)
+    x = torch.tensor([[1., 2.], [2.2, -0.5], [-10.3, 8.], [34.2, -8.5]], requires_grad=False)
+    y = torch.tensor([1., -1., 1., -1.], requires_grad=False)
+    lr = 0.1
+
+    flag = True
+    while flag:
+        flag = False
+        for i in range(len(x)):
+            pre_y = model(x[i])
+            if pre_y != y[i]:
+                model.w += lr * y[i] * x[i]
+                model.b += lr * y[i]
+                flag = True
+            print("--------" + str(i) + "--------")
+            print(model.w)
+            print(model.b)
+
+
+def book():
+    x = torch.tensor([[3., 3.], [4., 3.], [1., 1.]])
+    y = torch.tensor([1., 1., -1.])
+    w = torch.tensor([0. for _ in range(len(x[0]))], requires_grad=False)
+    b = torch.tensor(0., requires_grad=False)
+    model = PerceptronLiHangBook(w, b)
+    lr = 1.
+
+    flag = True
+    while flag:
+        flag = False
+        for i in range(len(x)):
+            if model(x[i]) * y[i] <= 0:
+                model.w += lr*y[i]*x[i]
+                model.b += lr*y[i]
+                flag = True
+                print("-------------" + str(i) + "-------------")
+                print(model.w)
+                print(model.b)
 
 def main():
-    w = 0.0
-
-    model = Perceptron(w)
+    # basic()
+    book()
 
 if __name__ == "__main__":
     main()
